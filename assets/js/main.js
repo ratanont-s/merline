32c476b9fb90b3bff27964ccae261febcc12ef43
rathanont <rathanont.s@gmail.com>
2019-09-19 09:14:07 +0700

    update code

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+    // Smooth scrolling
+    $("#navbarSupportedContent a.nav-link").on('click', function (event) {
+        var target = this.hash,
+            $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top - 70
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
